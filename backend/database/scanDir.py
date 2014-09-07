192f5883cbd906b2d3081c4eab12ce48f38c7ee2
andreasDL <andreasdelille@hotmail.Com>
2014-09-08 00:15:54 +0200

    bug in scanDir.py

--- backend/database/scanDir.py
+++ backend/database/scanDir.py
@@ -2,17 +2,14 @@ import sys
 import os
 
 dir  = ""
-type = ""
-if len(sys.argv) != 3:
-	print("Usage: python scanDir <directory> <type>")
-	print('using defaults: /mnt/data/done and movie')
+if len(sys.argv) != 2:
+	print("Usage: python scanDir <directory>")
+	print('using default: /mnt/data/done')
 	dir = "/mnt/data/done"
-	type = "movie"
 else:	
 	dir = sys.argv[1]
-	type = sys.argv[2]
 
-print("scanning",dir,"for",type)
+print("scanning",dir)
 
 output = open("output.sql", "w")
 output.write("USE mediaCenter;\ninsert into movies (name,moviePath,thumbPath) VALUES")
@@ -21,19 +18,17 @@ count = 0
 for f in os.listdir(dir):
 	name = f
 	path = dir + "/" + f
-	if not os.path.isdir(path):
-		print("Skipping" + path)
-		continue;
 
-	for i in os.listdir(path):
-		if i.endswith(".mkv") or i.endswith(".avi") or i.endswith(".wmi") or i.endswith("mp4"):
-			path = i
-			break;
+	if os.path.isdir(path):
+		for i in os.listdir(path):
+			if i.endswith(".mkv") or i.endswith(".avi") or i.endswith(".wmi") or i.endswith("mp4"):
+				path = path + "/" + i
+				break;
 
-	if (count > 0):
-		output.write( ",\\\n\t(\"" + name + "\",\"" + path + "\",\"" + "" + "\")")
-	else:
-		output.write(  "\\\n\t(\"" + name + "\",\"" + path + "\",\"" + "" + "\")")
-	count += 1
+		if (count > 0):
+			output.write( ",\\\n\t(\"" + name + "\",\"" + path + "\",\"" + "" + "\")")
+		else:
+			output.write(  "\\\n\t(\"" + name + "\",\"" + path + "\",\"" + "" + "\")")
+		count += 1
 
 output.write(";\n\n")
