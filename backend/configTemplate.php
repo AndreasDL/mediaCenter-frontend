d8f596a32e8e20883ada5ccba1050925caf63a7d
andreasDL <andreasdelille@hotmail.Com>
2014-09-09 18:54:11 +0200

    backend is now hardcoded

--- backend/configTemplate.php
+++
@@ -1,6 +0,0 @@
-<?php
-//settings
-global $locd; $locd = "localhost"; //location of database
-global $user; $user = ""; //username
-global $pass; $pass = ""; //password
-global $dbna; $dbna = "mediaCenter"; //database name
